f5a926f087ca10de613f91ee14b3240a9735a7ae
kenu <kenu.heo@gmail.com>
2012-04-24 14:47:37 +0900

    회원 목록 대외비

--- okhq/WebContent/member.jsp
+++ okhq/WebContent/member.jsp
@@ -1,9 +1,7 @@
-<%@page import="kr.pe.okjsp.member.MemberDao"%>
+<%@page import="kr.pe.okjsp.okhq.MemberDao"%>
 <%@page import="kr.pe.okjsp.member.Member"%>
 <%@page import="java.util.List"%>
-<%@page import="kr.pe.okjsp.okhq.AdLogStatDto"%>
-<%@page import="kr.pe.okjsp.okhq.AdLogStatDao"%>
-<%@ page language="java" contentType="text/html; charset="
+<%@ page language="java" contentType="text/html; charset=utf-8"
     pageEncoding="utf-8"%>
 <!DOCTYPE html>
 <html>
@@ -14,13 +12,32 @@
 </head>
 <body>
 <h1>member list</h1>
+<h2 class="warning">대외비</h2>
 <table>
-<tr><th>Name</th><th>Email</th></tr>
+<tr><th>#</th>
+<th>Name</th>
+<th>Email</th>
+<th>Mail</th>
+<th>가입일</th>
+<th>연번</th>
+<th>아이디</th>
+<th>포인트</th>
+</tr>
 <%
 	MemberDao dao = new MemberDao();
-	List<Member> list = dao.getList();
+	List<Member> list = dao.getList(500);
+	int count = 0;
 	for (Member row : list) {
-%><tr><td><%= row.getName() %></td><td class="number"><%= row.getEmail() %></td></tr>
+%><tr>
+<td><%= ++count %></td>
+<td><%= row.getName() %></td>
+<td><%= row.getEmail() %></td>
+<td><%= row.getMailing() %></td>
+<td><%= row.getJoindate() %></td>
+<td class="number"><%= row.getSid() %></td>
+<td><%= row.getId() %></td>
+<td><%= row.getPoint() %></td>
+</tr>
 <%
 	}
 %>
--- okhq/src/kr/pe/okjsp/okhq/MemberDao.java
+++ okhq/src/kr/pe/okjsp/okhq/MemberDao.java
@@ -3,6 +3,7 @@ package kr.pe.okjsp.okhq;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,21 @@ import kr.pe.okjsp.member.MemberHandler;
 import kr.pe.okjsp.util.DbCon;
 
 public class MemberDao extends MemberHandler {
-	private String AD_LOG_STAT = "select * from okmember order by 1 desc";
+	private String AD_LOG_STAT = "select * from okmember order by sid desc";
 	DbCon dbCon = new DbCon();
 
+	private Member getMember(ResultSet rs) throws SQLException {
+		Member row = new Member();
+		row.setName(rs.getString("name"));
+		row.setEmail(rs.getString("email"));
+		row.setMailing(rs.getString("mailing"));
+		row.setJoindate(rs.getTimestamp("joindate"));
+		row.setSid(rs.getLong("sid"));
+		row.setId(rs.getString("id"));
+		row.setPoint(rs.getLong("point"));
+		return row;
+	}
+	
 	public List<Member> getList() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -26,12 +39,32 @@ public class MemberDao extends MemberHandler {
 			pstmt = conn.prepareStatement(AD_LOG_STAT);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
-				Member row = new Member();
-				row.setName(rs.getString("name"));
-				row.setEmail(rs.getString("email"));
-				list.add(row);
+				list.add(getMember(rs));
 			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			dbCon.close(conn, pstmt, rs);
+		}
+		return list;
+	}
+
+	public List<Member> getList(int limit) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<Member> list = new ArrayList<Member>();
+		
+		try {
+			conn = dbCon.getConnection();
+			pstmt = conn.prepareStatement(AD_LOG_STAT + " limit "+ limit);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				list.add(getMember(rs));
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
